d1e33450e220a6048a460b33c605b85c89358a43
ompalsingh23 <deoraompal23@gmail.com>
2021-12-30 12:57:01 +0530

    final

---
+++ index.js
@@ -0,0 +1,17 @@
+// const menubtn = document.querySelector('.menubtn');
+// const menu_item = document.querySelectorAll('.nav-list ul li a');
+
+
+// // menubtn.addEventListener('click', () => {
+// // 	menubtn.classList.toggle('active');
+// // });
+
+
+// menu_item.forEach((item) => {
+// 	item.addEventListener('click', () => {
+// 		menubtn.classList.toggle('active');
+// 	});
+// 
+function show(){
+ document.getElementsByClassName("myname").classList.toggle('active');
+}
