9239a11e8a6e198ba4f8ac0f9865d216e1a634f4
upadhyayprateek <2019ume1715@mnit.ac.in>
2022-01-06 00:52:36 +0530

    pcprateek

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ var stts=1;
 function tgl(){
     if(stts==1){
         menu.style.right="0px";
+        menu.style.transition= "width 1s ease";
         stts=0;
     }
     else{
-        menu.style.right="-300px";
+        menu.style.right="-320px";
+        menu.style.transition= "width 1s ease";
         stts=1;
     }
 }
