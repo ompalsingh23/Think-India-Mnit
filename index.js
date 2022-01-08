d101e4cc0ac82612e1cc05e5ca7b7c4374968611
upadhyayprateek <2019ume1715@mnit.ac.in>
2022-01-08 23:20:51 +0530

    pcprateek2

--- index.js
+++ index.js
@@ -1,4 +1,74 @@
 
+
+/// for Events Section of Heritage Start 
+// for image 2
+function myfan2(){
+    var a = document.getElementById("slidep1")
+    var c = document.getElementById("slidep3")
+    var d = document.getElementById("slidep4")
+    var y = document.getElementById("slidep2")
+    if (a.style.display === "" || c.style.display === "" || d.style.display === "") {
+    a.style.display = "none";
+    c.style.display = "none";
+    d.style.display = "none";
+    y.style.display = "";
+    }
+    // else {
+//     x.style.display = "none";
+//   }
+}
+// for image 3
+function myfan3(){
+    var a = document.getElementById("slidep1")
+    var b = document.getElementById("slidep2")
+    var d = document.getElementById("slidep4")
+    var y = document.getElementById("slidep3")
+    if (a.style.display === "" || b.style.display === "" || d.style.display === "") {
+    a.style.display = "none";
+    b.style.display = "none";
+    d.style.display = "none";
+    y.style.display = "";
+    }
+    // else {
+//     x.style.display = "none";
+//   }
+}
+// for image 4
+function myfan4(){
+    var a = document.getElementById("slidep1")
+    var b = document.getElementById("slidep2")
+    var c = document.getElementById("slidep3")
+    var y = document.getElementById("slidep4")
+    if (a.style.display === "" || b.style.display === "" || c.style.display === "") {
+    a.style.display = "none";
+    b.style.display = "none";
+    c.style.display = "none";
+    y.style.display = "";
+    }
+    // else {
+//     x.style.display = "none";
+//   }
+}
+// for image 1
+function myfan1(){
+    var d = document.getElementById("slidep4")
+    var b = document.getElementById("slidep2")
+    var c = document.getElementById("slidep3")
+    var y = document.getElementById("slidep1")
+    if (d.style.display === "" || b.style.display === "" || c.style.display === "") {
+    d.style.display = "none";
+    b.style.display = "none";
+    c.style.display = "none";
+    y.style.display = "";
+    }
+    // else {
+//     x.style.display = "none";
+//   }
+}
+/// for Events Section of Heritage End
+
+
+// for menu btn
 var btn= document.getElementById("menubtn");
 var menu=document.getElementById("menu");
 var stts=1;
